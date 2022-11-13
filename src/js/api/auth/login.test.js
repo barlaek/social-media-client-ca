174bbd723730d2bfefbc45495ae3b18551f38b27
Liridona <91525128+barlaek@users.noreply.github.com>
2022-11-13 12:12:16 +0100

    testing login with jest

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,36 +1,67 @@
 import { login } from './login';
 
-const TEST_ID = 1;
-const TEST_BAD_ID = 'string';
-const TEST_ITEM = { id: TEST_ID, name: 'Test item' };
+class LsMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  remove(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LsMock();
+
+const TEST_USERNAME = 'test_bombadil';
+const TEST_EMAIL = 'bombadil_test@noroff.no';
+const TEST_PW = '12345678';
+const TEST_TOKEN = 'token123';
+const TEST_RESPONSE = {
+  name: TEST_USERNAME,
+  email: TEST_EMAIL,
+  token: TEST_TOKEN,
+};
 
 function fetchSuccess() {
   return Promise.resolve({
     ok: true,
     status: 200,
     statusText: 'OK',
-    json: () => Promise.resolve(TEST_ITEM),
+    json: () => Promise.resolve(TEST_RESPONSE),
   });
 }
 
-function fetchFailure(status = 404, statusText = 'Not found') {
+function fetchFailure() {
   return Promise.resolve({
     ok: false,
-    status,
-    statusText,
+    status: 401,
+    statusText: 'Unauthorized',
   });
 }
 
 describe('login', () => {
   it('Returns a valid object when provided with a valid ID', async () => {
     global.fetch = jest.fn(() => fetchSuccess());
-    const item = await login(1);
-    expect(item).toEqual(TEST_ITEM);
+    const item = await login(TEST_EMAIL, TEST_PW);
+    global.localStorage.setItem('token', TEST_TOKEN);
+    expect(item).toEqual(TEST_RESPONSE);
+    expect(item).toEqual(global.localStorage.getItem('token'));
   });
 
-  it('Returns undefined when an HTTP 404 error is received', async () => {
-    global.fetch = jest.fn(() => fetchFailure());
-    const item = await login(1);
-    expect(item).toEqual(undefined);
-  });
+  //   it('Throws a new error message at bad requests', async () => {
+  //     global.fetch = jest.fn(() => fetchFailure());
+  //     await expect(login(TEST_EMAIL, TEST_PW)).rejects.toThrow('Unauthorized');
+  //   });
 });
