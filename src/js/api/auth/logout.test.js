8b59d358bbb0cac9f125a717ca8a24ab0b6ef45c
Liridona <91525128+barlaek@users.noreply.github.com>
2022-11-27 18:12:01 +0100

    problems

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,3 +1,4 @@
+import { load, remove, save } from '../../storage/index';
 import { logout } from './logout';
 import { LsMock, TEST_TOKEN } from './login.test';
 
@@ -7,6 +8,17 @@ describe('logout', () => {
   it('Removes the valid user token on logout', () => {
     localStorage.setItem('token', JSON.stringify(TEST_TOKEN));
     logout();
-    expect(localStorage.getItem('token').toEqual(null));
+    expect(localStorage.getItem('token').toEqual(undefined));
   });
 });
+
+// describe('logout', () => {
+//   it('removes an item from the storage', () => {
+//     const key = 'user';
+//     const value = ['email', 'pw'];
+//     global.localStorage.setItem(key, value);
+//     expect(global.localStorage.load(key)).toEqual(value);
+//     global.localStorage.removeItem(key);
+//     expect(global.localStorage.load(key)).toEqual(undefined);
+//   });
+// });
